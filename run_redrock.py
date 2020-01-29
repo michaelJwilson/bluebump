2d16622e5658a36a958b74fd8dfa6e13b8916b9e
michaelJwilson <mjwilson@lbl.gov>
2020-01-29 14:08:32 -0800

    Upate.

--- run_redrock.py
+++ run_redrock.py
@@ -2,13 +2,24 @@ import os
 import glob
 
 
-files = glob.glob('/global/cscratch1/sd/mjwilson/desi/bluebump/degraded/*')
-files.remove('/global/cscratch1/sd/mjwilson/desi/bluebump/degraded/redrock')
+# S/N degraded. 
+# files = glob.glob('/global/cscratch1/sd/mjwilson/desi/bluebump/degraded/*')
+# files.remove('/global/cscratch1/sd/mjwilson/desi/bluebump/degraded/redrock')
+
+# Redwood rerun. 
+file  = open('redwood_spectra_files.txt', mode = 'r')
+files = file.readlines()
+files = [x.replace('\n', '') for x in files]
+file.close()
 
 for i, x in enumerate(files):
-  _     = x.split('/')[-1].split('-')[2].replace('.fits', '')
-  ofile = os.environ['CSCRATCH'] + '/desi/bluebump/degraded/redrock/redrock-64-{}.h5'.format(_)
-  zbest = os.environ['CSCRATCH'] + '/desi/bluebump/degraded/redrock/zbest-64-{}.fits'.format(_)
+  _     = x.split('/')[-1]
+  x     = x + '/spectra-64-{}.fits'.format(_)
+
+  # _   = x.split('/')[-1].split('-')[2].replace('.fits', '')
+
+  ofile = os.environ['CSCRATCH'] + '/desi/bluebump/redwood/redrock/redrock-64-{}.h5'.format(_)
+  zbest = os.environ['CSCRATCH'] + '/desi/bluebump/redwood/redrock/zbest-64-{}.fits'.format(_)
   
   if os.path.exists(ofile):
     print('{} exists.'.format(ofile))
