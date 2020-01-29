5aeb45ab19d32f5a6cc82b5047d7fd4fe912cd36
michaelJwilson <mjwilson@lbl.gov>
2020-01-29 15:13:50 -0800

    Update.

--- run_redrock.py
+++ run_redrock.py
@@ -3,23 +3,25 @@ import glob
 
 
 # S/N degraded. 
-# files = glob.glob('/global/cscratch1/sd/mjwilson/desi/bluebump/degraded/*')
-# files.remove('/global/cscratch1/sd/mjwilson/desi/bluebump/degraded/redrock')
+run   = 'degraded'
+files = glob.glob('/global/cscratch1/sd/mjwilson/desi/bluebump/degraded/*')
+files.remove('/global/cscratch1/sd/mjwilson/desi/bluebump/degraded/redrock')
 
-# Redwood rerun. 
-file  = open('redwood_spectra_files.txt', mode = 'r')
-files = file.readlines()
-files = [x.replace('\n', '') for x in files]
-file.close()
+# Redwood rerun.
+# run   = 'redwood'
+# file  = open('redwood_spectra_files.txt', mode = 'r')
+# files = file.readlines()
+# files = [x.replace('\n', '') for x in files]
+# file.close()
 
 for i, x in enumerate(files):
-  _     = x.split('/')[-1]
-  x     = x + '/spectra-64-{}.fits'.format(_)
+  #_     = x.split('/')[-1]
+  #x     = x + '/spectra-64-{}.fits'.format(_)
 
-  # _   = x.split('/')[-1].split('-')[2].replace('.fits', '')
+  _     = x.split('/')[-1].split('-')[2].replace('.fits', '')
 
-  ofile = os.environ['CSCRATCH'] + '/desi/bluebump/redwood/redrock/redrock-64-{}.h5'.format(_)
-  zbest = os.environ['CSCRATCH'] + '/desi/bluebump/redwood/redrock/zbest-64-{}.fits'.format(_)
+  ofile = os.environ['CSCRATCH'] + '/desi/bluebump/{}/redrock/redrock-64-{}.h5'.format(run, _)
+  zbest = os.environ['CSCRATCH'] + '/desi/bluebump/{}/redrock/zbest-64-{}.fits'.format(run, _)
   
   if os.path.exists(ofile):
     print('{} exists.'.format(ofile))
--- zeff.py
+++ zeff.py
@@ -2,37 +2,54 @@ import os
 import numpy as np
 import pylab as pl
 
-import astropy.io.fits as fits
+import astropy.io.fits   as fits
+import matplotlib.style  as style
+import matplotlib.pyplot as plt
 
 
-root  = '/global/projecta/projectdirs/desi/datachallenge/redwood/'
-truth = root + '/targets/truth.fits'
+style.use('bmh')
 
-oput  = os.environ['CSCRATCH'] + '/desi/bluebump/zbest-64-10323.fits'
+root    = '/global/projecta/projectdirs/desi/datachallenge/redwood/'
+truth   = root + '/targets/truth.fits'
 
-dat   = fits.open(truth)[1]
+dat     = fits.open(truth)[1]
 
-print(dat.data)
+tids    = dat.data['TARGETID'] 
+tzs     = dat.data['TRUEZ']
 
-tids  = dat.data['TARGETID'] 
-tzs   = dat.data['TRUEZ']
+#
+degrade = os.environ['CSCRATCH'] + '/desi/bluebump/degraded/redrock/zbest-64-8579.fits'
+redwood = os.environ['CSCRATCH'] + '/desi/bluebump/redwood/redrock/zbest-64-8579.fits'
 
-zbest = fits.open(oput)[1]
-ztids = zbest.data['TARGETID']
-zz    = zbest.data['Z']
-zerr  = zbest.data['ZERR']
-zwarn = zbest.data['ZWARN']
+shift   = {'r': 0.025, 'b': 0.0}
 
-index         = np.argsort(tids)
-sorted_tids   = tids[index]
-sorted_index  = np.searchsorted(sorted_tids, ztids)
+for x, color, label in zip([degrade, redwood], ['r', 'b'], ['Degraded', 'Original']):
+  zbest = fits.open(x)[1]
+  ztids = zbest.data['TARGETID']
+  zz    = zbest.data['Z']
+  zerr  = zbest.data['ZERR']
+  zwarn = zbest.data['ZWARN']
 
-yindex        = np.take(index, sorted_index, mode="clip")
-mask          = tids[yindex] != ztids
+  index         = np.argsort(tids)
+  sorted_tids   = tids[index]
+  sorted_index  = np.searchsorted(sorted_tids, ztids)
 
-result        = np.ma.array(yindex, mask=mask)
+  yindex        = np.take(index, sorted_index, mode="clip")
+  mask          = tids[yindex] != ztids
 
-pl.plot(tzs[result], zz, '.')
+  result        = np.ma.array(yindex, mask=mask)
+
+  pl.plot(tzs[result] + shift[color], zz, 'x', c=color, markersize=3, label=label)
+
+pl.xlabel(r'$z_{\rm{True}}$')
+pl.ylabel(r'$\hat z$')
+pl.legend(loc=2, frameon=False)
+
+ax = pl.gca()
+
+ax.grid(False)
+
+plt.tight_layout()
 
 pl.savefig('zeff.pdf')
 
