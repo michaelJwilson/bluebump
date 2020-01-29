1cceaa96f8c71893c0055049261992d3eb57fa5e
michaelJwilson <mjwilson@lbl.gov>
2020-01-29 14:25:52 -0800

    Update.

--- run_qspectra.py
+++ run_qspectra.py
@@ -9,7 +9,7 @@ lines = [x.replace('\n','') for x in lines]
 exps  = [x.split('/')[-1] for x in lines]
 lines = [x + '/truth-64-{}.fits'.format(x.split('/')[-1]) for x in lines]
 
-for i, x in enumerate(lines):
+for i, x in enumerate(linesx):
   ofile = os.environ['CSCRATCH'] + '/desi/bluebump/spectra-64-{}.fits'.format(exps[i])
 
   if os.path.exists(ofile):
