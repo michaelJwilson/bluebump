5df51c280f153f51c4e2c43d23570b349347beaf
michaelJwilson <mjwilson@lbl.gov>
2020-02-04 14:23:19 -0800

    Update.

--- zeff.py
+++ zeff.py
@@ -23,10 +23,10 @@ root    = '/global/projecta/projectdirs/desi/datachallenge/redwood/'
 truth   = root + '/targets/truth.fits'
 
 truth   = Table.read(truth, format='fits')
-truth   = truth.to_pandas() 
+# truth = truth.to_pandas() 
 
-tids    = truth['TARGETID'] 
-tzs     = truth['TRUEZ']
+tids    = truth['TARGETID'] # .to_numpy()
+tzs     = truth['TRUEZ']    # .to_numpy()
 
 print(len(truth))
 print(truth)
@@ -126,29 +126,42 @@ for i, tile in enumerate(tiles):
     print('Number of redshifts in err by 1 sigma: {}'.format(np.count_nonzero(result[label]['signif'] > 1.0)))
     print('Number of redshifts in err by 2 sigma: {}'.format(np.count_nonzero(result[label]['signif'] > 2.0)))
     print('Number of redshifts in err by 3 sigma: {}'.format(np.count_nonzero(result[label]['signif'] > 3.0)))
-
+    
   # Check that no funny sorting happened between the Master and Degraded runs. 
   assert  np.all(result['Degraded']['ztids'] == result['Master']['ztids'])
 
   #
-  zzmask  =  result['Master']['zzmask']
-  warning = (result['Master']['zwarn'][zzmask] > 0)
+  zzmask       =  result['Master']['zzmask']
+  warning      = (result['Master']['zwarn'][zzmask] > 0)
 
-  pl.axhline(y=0.0, xmin=0, xmax=1, c='k')
-  pl.axhline(y=1.0, xmin=0, xmax=1, c='k')
+  sig          =  significance(result['Degraded']['zz'], result['Master']['zz'], result['Master']['zerr'])[zzmask][~warning]
+  band         =  sig > 0.0
+
+  mids         =  result['Master']['ztids'][zzmask][~warning]
   
   pl.plot(truzs[warning],  significance(result['Degraded']['zz'], result['Master']['zz'], result['Master']['zerr'])[zzmask][warning], ' x', c='r', markersize=3) 
   pl.plot(truzs[~warning], significance(result['Degraded']['zz'], result['Master']['zz'], result['Master']['zerr'])[zzmask][~warning], 'x', c='k', markersize=3)
 
-  '''
-  sig          =  significance(result['Degraded']['zz'], result['Master']['zz'], result['Master']['zerr'])[zzmask][~warning] 
-  band         =  sig > 0
-  
-  isin         =  np.isin(truth['TARGETID'], result['Master']['tids'][zzmask])
+  pl.plot(truzs[~warning][band], significance(result['Degraded']['zz'], result['Master']['zz'], result['Master']['zerr'])[zzmask][~warning][band], 'x', c='b', markersize=3)
 
-  print(np.count_nonzero(isin))
-  print(truth[isin])
-  '''
+  #  
+  isin         =  np.isin(truth['TARGETID'], mids)
+  _            =  Table(truth[isin], copy=True)
+
+  _.sort('TARGETID')
+
+  inds         =  np.argsort(mids)
+
+  _['SIG']     =  sig[inds]
+
+  print()
+  print(_[_['SIG'] > 0.0])
+  
+  # break
+
+#
+pl.axhline(y=0.0, xmin=0, xmax=1, c='k')
+pl.axhline(y=1.0, xmin=0, xmax=1, c='k')
   
 pl.xlim(-0.02, 1.7)
 pl.ylim(-2.00, 5.0)
@@ -156,7 +169,7 @@ pl.ylim(-2.00, 5.0)
 pl.xlabel(r'$z_{\rm{True}}$')
 pl.ylabel(r"$\log_{10}(|z' - z| \ / \ z_{\rm{err}})$")
 
-pl.legend(loc=1, frameon=True)
+# pl.legend(loc=1, frameon=True)
 
 ax = pl.gca()
 
